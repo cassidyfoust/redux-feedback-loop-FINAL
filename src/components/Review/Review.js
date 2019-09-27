328d7facb615fadd64e1d764f5482385109294be
Cassidy Foust <cassidy.foust@gmail.com>
2019-09-27 18:37:08 -0500

    added all routers and pages git push origin master

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { Button, createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+    overrides: {
+        MuiButton: {
+            root: {
+                margin: "10px",
+                padding: "10px"
+            }
+        }
+    }
+});
 
 class Review extends Component {
 
+    handleClick = () => {
+        console.log('clicked next!')
+        // this.props.history.push('/review')
+    }
+
     render() {
         return (
-            <div>
-                <h2>Review</h2>
-
-            </div>
+            <>
+                <MuiThemeProvider theme={theme}>
+                    <div className="component">
+                        <h2>Review Your Feedback</h2>
+                    </div>
+                    <div className="summary">
+                       Feelings:
+                       <br></br>
+                       <br></br>
+                       Understanding:
+                       <br></br>
+                       <br></br>
+                       Support:
+                       <br></br>
+                        <br></br>
+                        Comments:
+                        </div>
+                        <div className="component">
+                        <Button className="btn" variant="contained" color="primary" onClick={this.handleClick}>
+                            Submit
+                    </Button>
+                    </div>
+                </MuiThemeProvider>
+            </>
         )
     };
 }
--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { Button, createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+    overrides: {
+        MuiButton: {
+            root: {
+                margin: "10px",
+                padding: "10px"
+            }
+        }
+    }
+});
 
 class Support extends Component {
 
+    handleClick = () => {
+        console.log('clicked next!')
+        this.props.history.push('/comments')
+    }
+
     render() {
         return (
-            <div>
-                <h2>Support</h2>
-
-            </div>
+            <>
+                <MuiThemeProvider theme={theme}>
+                    <div className="component">
+                        <h2>How well are you being supported?</h2>
+                    </div>
+                    <div className="component">
+                        <TextField
+                            id="standard-number"
+                            label="Support?"
+                            // value={values.age}
+                            // onChange={handleChange('age')}
+                            type="number"
+                            // className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            margin="normal"
+                            required={true}
+                        />
+                        <Button className="btn" variant="contained" color="primary" onClick={this.handleClick}>
+                            Next
+                    </Button>
+                    </div>
+                </MuiThemeProvider>
+            </>
         )
     };
 }
